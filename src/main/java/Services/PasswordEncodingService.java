e53c64b2bcc1d103f7122318211ab698a0d70f65
Yevhen_Lazhyntsev <lazhynce@gmail.com>
2020-02-15 20:13:13 +0200

    fixup! BCrypt encription provided

---
+++ src/main/java/Services/PasswordEncodingService.java
@@ -0,0 +1,8 @@
+package Services;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+public interface PasswordEncodingService extends PasswordEncoder {
+}
